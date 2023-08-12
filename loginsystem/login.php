10ebb783d5a6b351ad24871222c77999e354111c
Roobon <roobon@gmail.com>
2023-08-12 13:29:37 +0600

    Class work 12 August 2023

--- loginsystem/login.php
+++ loginsystem/login.php
@@ -4,18 +4,21 @@
     if(isset($_POST['submit'])){
         $email = $_POST['email'];
         $pass = $_POST['password'];
+        $pass = sha1($pass); // encrypted
 
         require_once("db_config.php");
 
-        $sql = "SELECT email, password FROM users WHERE email='$email' AND password = '$pass'";
+        $sql = "SELECT name, email, password FROM users WHERE email='$email' AND password = '$pass'";
         $result = $db->query($sql);
+        $row = $result->fetch_assoc();
         session_start();
 
         if($result->num_rows){
-            
+            $_SESSION['name'] = $row['name'];
+            $_SESSION['email'] = $row['email'];
             header("Location: home.php");
         } else {
-            $_SESSION['error'] = "Email and password is not stored in the database";
+            $_SESSION['error'] = "Email and password is not stored in the database<br>";
             header("Location: index.php");
         }
        
