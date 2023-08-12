a84d79a80eeaaf88cbcab5d15b5ed872e948688d
ikbalhossainn <ikbalhossain125@gmail.com>
2023-08-12 09:45:10 +0600

    updated

---
+++ Ikbal_Hossain_1277699/1_login_system.php/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2> Login Form </h2>
+    <form action="0">
+        <input type="email" name="email" placeholder="Enter Email"> <br>
+        <input type="password" name="password" placeholder="Enter password"> <br>
+        <input type="submit" name="submit" value="LOgIN"> <br>
+    </form>
+</body>
+</html>
