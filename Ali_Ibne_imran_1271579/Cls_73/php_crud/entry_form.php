d5390abeec6fdb952c9780cfd2fb4341fbd1e9ca
unknown <you@example.com>
2023-08-13 13:22:18 +0600

    add

--- Ali_Ibne_imran_1271579/Cls_73/php_crud/entry_form.php
+++ Ali_Ibne_imran_1271579/Cls_73/php_crud/entry_form.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Edit</title>
+  <title>Person Entry</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -10,18 +10,13 @@
 <body>
 
 <div class="container mt-3">
-  <h2>Edit</h2>
-    <?php
-        require_once("db_config.php");
-        $id = $_GET["id"];
-        $result = $db->query("SELECT * FROM persons WHERE PersonID='$id'");
-        $row = $result->fetch_assoc();
-        
+  <h2>Person Entry Form</h2>
+    <?php 
         if(isset($_POST["entry"])){
             extract($_POST);
-            
+            require_once("db_config.php");
             // echo "INSERT INTO persons VALUE (NULL, '$lname','$fname','$address','$city',$email','$dob')";
-            // $sql = "INSERT INTO persons VALUE (NULL, '$lname','$fname','$address','$city','$email','$dob')";
+            $sql = "INSERT INTO persons VALUE (NULL, '$lname','$fname','$address','$city','$email','$dob')";
             $db->query($sql);
 
             if($db->affected_rows){
@@ -37,34 +32,34 @@
   <form action="<?php echo $_SERVER["PHP_SELF"]?>" method="post">
     <div class="mb-3 mt-3">
       <label for="fname">First Name:</label>
-      <input type="text" class="form-control" name="fname" value="<?php echo $row['FirstName']?>">
+      <input type="text" class="form-control" name="fname">
     </div>
     <div class="mb-3 mt-3">
       <label for="lname">Last Name:</label>
-      <input type="text" class="form-control" name="lname" value="<?php echo $row['LastName']?>">
+      <input type="text" class="form-control" name="lname">
     </div>
     <div class="mb-3 mt-3">
       <label for="address">Address:</label>
-      <textarea class="form-control" name="address" value="<?php echo $row['Address']?>"></textarea>
+      <textarea class="form-control" name="address"></textarea>
     </div>
     <div class="mb-3 mt-3">
       <label for="city">City:</label>
-      <input type="text" class="form-control" name="city" value="<?php echo $row['City']?>">
+      <input type="text" class="form-control" name="city">
     </div>
     <div class="mb-3 mt-3">
       <label for="email">Email:</label>
-      <input type="email" class="form-control" name="email" value="<?php echo $row['email_address']?>">
+      <input type="email" class="form-control" name="email">
     </div>
     <div class="mb-3 mt-3">
       <label for="dob">Dob:</label>
-      <input type="date" class="form-control" name="dob" value="<?php echo $row['dob']?>">
+      <input type="date" class="form-control" name="dob">
     </div>
     <!-- <div class="form-check mb-3">
       <label class="form-check-label">
         <input class="form-check-input" type="checkbox" name="remember"> Remember me
       </label>
     </div> -->
-    <button type="submit" class="btn btn-success" name="entry">Update</button>
+    <button type="submit" class="btn btn-success" name="entry">Entry</button>
   </form>
 
 </div>
