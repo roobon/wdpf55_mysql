705991507e0c16fa8d4bcce2314bce4a3343e2ac
ikbalhossainn <ikbalhossain125@gmail.com>
2023-08-26 12:39:34 +0600

    updated

---
+++ Ikbal_Hossain_1277699/Class_Practice/Evidence/company_product_list.php
@@ -0,0 +1,21 @@
+<?php
+$db =  new mysqli("localhost", "root", "", "wdpf55_evidence");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <h3> Company List </h3>
+    <form action="">
+          <select name="" id=""> Select one </select>
+    </form>
+</body>
+
+</html>
