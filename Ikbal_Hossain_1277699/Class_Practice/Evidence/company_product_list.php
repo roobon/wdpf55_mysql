5ea3d3bdbd6f35e737d2dfed8b68dc6fc7503ffa
ikbalhossainn <ikbalhossain125@gmail.com>
2023-08-26 12:52:53 +0600

    updated

--- Ikbal_Hossain_1277699/Class_Practice/Evidence/company_product_list.php
+++ Ikbal_Hossain_1277699/Class_Practice/Evidence/company_product_list.php
@@ -13,8 +13,29 @@ $db =  new mysqli("localhost", "root", "", "wdpf55_evidence");
 
 <body>
   <h3> Company List </h3>
-    <form action="">
-          <select name="" id=""> Select one </select>
+
+  <?php 
+    if(isset($_POST['delete'])) :
+
+      print_r($_POST);
+
+    endif;
+  ?>
+    <form action="" method="post">
+          <select name="company">
+            <option value=""> Select One </option>
+
+            <?php 
+              $result = $re -> query("SELECT * FROM manufacturer");
+              while($row = $result -> fetch_assoc()) :
+            ?>
+
+            <option value=""><?php echo $row['company_name']; ?></option>
+
+            <?php endwhile;?>
+        </select>
+
+        <input type="submit" name="delete" value="DELETE">
     </form>
 </body>
 
