2047fe24f9ab3a92b2301a769a33e746c26a54d4
Alauddin007-hub <mdalauddinalsafin@gmail.com>
2023-08-26 22:10:50 +0600

    dxrgt

--- Alauddin_1273850/php_crud1/edit.php
+++ Alauddin_1273850/php_crud1/edit.php
@@ -23,19 +23,23 @@
             header("Location:index.php");
         }
     }
+
+        $result = $db->query("SELECT * FROM persons WHERE ID='$id'");
+        $row = $result->fetch_assoc();
+
     ?>
     <form action="" method="post">
         <label for="">Name: </label><br>
-        <input type="text" name="name" placeholder="Enter your name"><br>
+        <input type="text" name="name" placeholder="Enter your name" value="<?php echo $row['Name']?>"><br>
 
         <label for="">Gender: </label><br>
         <label for="">Male</label>
-        <input type="radio" name="gender" value="male">
+        <input type="radio" name="gender" value="male" <?php if($row['gender'] == 'male') echo "selected";?>>
         <label for="">Female</label>
-        <input type="radio" name="gender" value="female"><br>
+        <input type="radio" name="gender" value="female" value="male" <?php if($row['gender'] == 'female') echo "selected";?>><br>
 
         <label for="">Date of Birth</label><br>
-        <input type="date" name="dob"><br>
+        <input type="date" name="dob" value="<?php echo $row['DOB']?>"><br>
 
         <label for="">Email Address</label><br>
         <input type="email" name="email" placeholder="Enter your email"><br>
@@ -43,14 +47,14 @@
         <label for="">Batch</label><br>
         <select name="batch">
             <option value="">Select one</option>
-            <option value="WDPF-55">WDPF-55</option>
-            <option value=".NET-53">.NET-53</option>
-            <option value="JAVA-52">JAVA-52</option>
-            <option value="GAVE-54">GAVE-54</option>
+            <option value="WDPF-55"  <?php if($row['batch'] == 'WDPF-55') echo "selected";?>>WDPF-55</option>
+            <option value=".NET-53" <?php if($row['batch'] == '.NET-53') echo "selected";?>>.NET-53</option>
+            <option value="JAVA-52" <?php if($row['batch'] == 'JAVA-52') echo "selected";?>>JAVA-52</option>
+            <option value="GAVE-54" <?php if($row['batch'] == 'GAVE-54') echo "selected";?>>GAVE-54</option>
         </select><br>
 
         <label for="">Address</label><br>
-        <textarea name="address" id="" cols="30" rows="10"></textarea><br>
+        <textarea name="address" id="" cols="30" rows="10"><?php echo $row['Address']?></textarea><br>
 
         <label for="">Hobbies</label><br>
         <label for="">Cricket</label>
