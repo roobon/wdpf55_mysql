1225a2a11bf04f3fdecf217e9cbd613f937d4eaf
ikbalhossainn <ikbalhossain125@gmail.com>
2023-08-27 11:42:56 +0600

    updated

--- Ikbal_Hossain_1277699/Evidence/company_product_list.php
+++ Ikbal_Hossain_1277699/Evidence/company_product_list.php
@@ -8,7 +8,9 @@ $db =  new mysqli("localhost", "root", "", "wdpf55_evidence");
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
   <title>Document</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 
 <body>
@@ -23,7 +25,7 @@ $db =  new mysqli("localhost", "root", "", "wdpf55_evidence");
 
   endif;
   ?>
-  
+
   <!-- step: 01 => show form and drop down list in company_name -->
   <form action="" method="post">
       <select name="company">
@@ -41,7 +43,7 @@ $db =  new mysqli("localhost", "root", "", "wdpf55_evidence");
           <?php endwhile; ?>
       </select> <br>
 
-      <input type="submit" name="delete" value="DELETE">
+      <input type="submit" name="delete" class="btn btn-danger" value="DELETE">
   </form>
 
   <h3> Product List </h3>
@@ -75,10 +77,22 @@ $db =  new mysqli("localhost", "root", "", "wdpf55_evidence");
        endwhile;
     ?>
 
-  </table>
+  </table> <br>
+
+  <a href="manufacturer_entry.php" class="btn btn-success">Manufacturer Entry </a>
 
 
 
+
+
+
+
+
+
+
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 
 </html>
