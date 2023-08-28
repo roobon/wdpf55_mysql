ca04c2d27131e406fea9aeb4d0c2abb407fac941
Hasnain Sadid <hasnainsadid@gmail.com>
2023-08-28 23:49:04 +0600

    added login_Sadid

--- Sadid_1273829/Curd_1/entry.php
+++ Sadid_1273829/Curd_1/entry.php
@@ -21,7 +21,6 @@ if (isset($_REQUEST['submit'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <title>New Entry</title>
 </head>
-<<<<<<< HEAD
 <body>
 <form class="container w-25 mt-5">
     <input type="name" name="name" class="form-control" placeholder="Enter name"><br>
@@ -46,41 +45,6 @@ if (isset($_REQUEST['submit'])) {
     </select><br>
     <input type="submit" class="btn btn-success m-auto d-block w-50" name="submit" value="Add New">
 </form>
-=======
->>>>>>> 42475f072811c86244bf2551b49cd584bfedb23b
-
-<body>
-    <form class="container w-25 mt-5">
-        <input type="name" name="name" class="form-control" placeholder="Enter name"><br>
-
-        Gender:
-        <?php  ?>
-        <input type="radio" class="" name="gender" value="Male"> Male
-        <input type="radio" class="" name="gender" value="Female"> Female <br>
-
-        <input type="date" name="dob" class="form-control"><br>
-
-        <input type="email" name="email" class="form-control" placeholder="enter email"><br>
-
-        Batch:
-        <select name="batch">
-            <option value="53">53</option>
-            <option value="54">54</option>
-            <option value="55">55</option>
-            <option value="56">56</option>
-        </select> <br>
-
-        <textarea name="address" class="form-control" placeholder="Enter address"></textarea> <br>
-
-        Hobbies: <br>
-        <input type="checkbox" name="hobby[]" value="cricket"> Cricket <br>
-        <input type="checkbox" name="hobby[]" value="football"> Football <br>
-        <input type="checkbox" name="hobby[]" value="hockey"> Hockey <br>
-        <input type="checkbox" name="hobby[]" value="tenis"> Tenis <br>
-        </select><br>
-
-        <input type="submit" class="btn btn-success m-auto d-block w-50" name="submit" value="Add New">
-    </form>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
