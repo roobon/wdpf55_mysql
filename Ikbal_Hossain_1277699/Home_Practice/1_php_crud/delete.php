4158fa3a037f8dd63c152d8358bf7ab9e8b10efc
Ikbal Hossain <ikbalhossain125@gmail.com>
2023-08-29 18:20:03 +0600

    updated

--- Ikbal_Hossain_1277699/Home_Practice/1_php_crud/delete.php
+++ Ikbal_Hossain_1277699/Home_Practice/1_php_crud/delete.php
@@ -1,11 +1,13 @@
 <?php 
     require_once("db_config.php");
 
-    echo $_GET["id"];
+    // echo $_GET["id"];
 
-    // $id = $_GET["id"];
+    $id = $_GET["id"];
 
     $sql = ( "DELETE FROM students_info WHERE student_id = '$id'" );
     $db -> query($sql);
-    // header("Location: 1_index.php");
+    // $db -> query("DELETE FROM students_info WHERE student_id = '$id'");
+    
+     header("Location: 1_index.php");
 ?>
