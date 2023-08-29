03b36980b5505621aa10f83c6187e15a6bf2f58c
mahdiulh <mahdiulh@gmail.com>
2023-08-30 02:50:06 +0600

    evidencepractice

---
+++ Mahdiul_Haque_1274133/mysql_evidence_practice/entryformmanufacture.php
@@ -0,0 +1,44 @@
+<?php 
+    $db  =  new mysqli("localhost", "root", "", "evidence");
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>New Product Entry</h2>
+    <?php 
+    if(isset($_POST['submit'])){
+        extract($_REQUEST);
+
+
+        $sql = "CALL insert_company_info('$name', '$address', '$contact_no')";
+        // $sql = "INSERT INTO manufacturer VALUES(NULL, '$company_name', '$address', '$contact_no')";
+        $db->query($sql);
+        if($db->affected_rows>0){
+            echo "Success";
+            header("Location: companylist.php");
+        }
+    }
+    
+    ?>
+    <a href="companylist.php">Company and Product List</a>
+    <form action="" method="post">
+        Company Name : <input type="text" name="name" placeholder="Enter Company Name"><br><br>
+        Address : <textarea name="address" id="" cols="30" rows="10"></textarea><br><br>
+        Contact No: <input type="number" name="contact_no"><br><br>
+        
+      
+        <input type="submit" name="submit" value="SUBMIT">
+        
+
+
+
+    </form>
+</body>
+</html>
