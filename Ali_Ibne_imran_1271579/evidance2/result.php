e8071cd52bd95f36cf26f7b89971a31e6da2162b
unknown <you@example.com>
2023-08-30 09:32:32 +0600

    add

---
+++ Ali_Ibne_imran_1271579/evidance2/result.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <form action="" method="post">
+            <div class="form-group mb-3 mt-3">
+                <label>Student Name :</label>
+                <input type="text" class="form-control" name="name">
+            </div>
+            <div class="form-group mb-3 mt-3">
+                <label>Address :</label>
+                <textarea class="form-control" name="address"></textarea>
+            </div>
+            <div class="form-group mb-3 mt-3">
+                <label>Phone :</label>
+                <textarea class="form-control" name="contact"></textarea>
+            </div>
+            
+            
+            <button type="submit" class="btn btn-success" name="entry">Entry</button>
+        </form>
+    </div>
+</body>
+</html>
---
+++ Ali_Ibne_imran_1271579/evidance2/student.php
@@ -0,0 +1,49 @@
+<?php  $db= new mysqli("localhost","root","","idb_bisew");?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+</head>
+<body>
+    <?php 
+       
+        if(isset($_POST["delete"])){
+            $mid = $_POST["students"];
+            $db->query("DELETE FROM student WHERE student_id='$mid'");
+            if($db->affected_rows){
+                echo "DELETED";
+            }
+
+        }
+      
+        
+    ?>
+    <div class="container">
+        <h1>Student</h1>
+        <form action="" method="post">
+
+            <div class="form-group mb-3 mt-3">
+                <select name="students" class="form-control">
+                   
+                    <option value="">Select One</option>
+                    <?php  
+                          $result = $db->query("SELECT * FROM student");while($row = $result->fetch_assoc()):
+                    ?>
+                    <option><?php echo $row["id"]." "?><?php echo $row["name"]?></option>
+                    <?php endwhile?>
+                </select>
+            </div>
+            <button type="submit" class="form-control btn btn-success" name="delete">Delete</button>
+        </form>
+
+
+
+          
+    </div>
+    
+</body>
+</html>
