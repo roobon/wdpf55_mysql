6ec5a6ad2da044f0c73c815ccb3109fdfa3a7f20
Mizanur Rahman <mizanur1326@gmail.com>
2023-08-30 12:30:50 +0600

    mid exam

--- Mizanur_Rahman_1277622/exam/entry.php
+++ Mizanur_Rahman_1277622/exam/entry.php
@@ -1,5 +1,5 @@
 <?php 
-    $db = new mysqli("localhost", "root", "", "evidence_1");
+    $db = new mysqli("localhost", "root", "", "exam");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,9 +9,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
     <title>Entry Form</title>
 </head>
 
@@ -21,17 +18,14 @@
     if (isset($_POST["submit"])) {
         extract($_POST);
         //print_r($_POST);
-        //echo "INSERT INTO manufacturar VALUES(NULL,  '$company', '$address', '$contact')";
-        // -------PROCEDURE E eikhane INSERT thakbe na Database er routine e thakbe//-----------
-        //$sql = "INSERT INTO manufacturar VALUES(NULL,  '$company', '$address', '$contact')";
         $sql = "CALL insert_company_info('$company', '$address', '$contact')";
         $db->query($sql);
     }
     ?>
-    <h3>Manufacturar Entry Form</h3>
+    <h3>Manufacturer Entry Form</h3>
     <form action="" method="post">
         <label for="company_name" class="form-label">Company Name :</label>
-        <input type="text" name="company" class="form-control" placeholder="Enter Manufacturar Company Name">
+        <input type="text" name="company" class="form-control" placeholder="Enter Manufacturer Company Name">
 
         <label for="address" class="form-label">Address :</label>
         <input type="text" name="address" class="form-control" placeholder="Enter Address">
@@ -46,9 +40,9 @@
     if (isset($_POST["submit"])) {
 
             if($db->affected_rows>0){
-                echo "<b>Manufacturar Successfully Added.</b>";
+                echo "<b>Manufacturer Successfully Added.</b>";
             }else{
-                echo "Failed to add new Manufacturar.";
+                echo "Failed to add new Manufacturer.";
             }
         }
     ?>
