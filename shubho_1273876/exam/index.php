d6521cb0fc010b9c6e40eae8da325e9034722d57
ikbalhossainn <ikbalhossain125@gmail.com>
2023-08-30 13:06:11 +0600

    updated

--- shubho_1273876/exam/index.php
+++ shubho_1273876/exam/index.php
@@ -1,5 +1,4 @@
 <?php 
-//require_once("db_config.php");
 
 $db = new mysqli("localhost", "root", "", "evidence_1");
 ?>
@@ -8,21 +7,21 @@ $db = new mysqli("localhost", "root", "", "evidence_1");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
-    <title>Form</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+
+    <title>Manufacturar List Form</title>
 </head>
 <body>
     <div class="container">
-    <h3>Manufacturar List</h3>
+        <div class="row">
+            <div class="col-md-12 mt-3 mb-3">    
+                <h3 style="text-align:center;">Manufacturar List</h3>
+            </div>
+        </div>
     </div>
 
     <?php 
     if(isset($_POST["delete"])):
-        //print_r($_POST);
         $id = $_POST["company"];
         $db->query("DELETE FROM manufacturar WHERE mid = $id");
 
@@ -35,7 +34,8 @@ $db = new mysqli("localhost", "root", "", "evidence_1");
 
     <div class="container">
         <form action="" method="post">
-            <select name="company" class="form-select">
+            <div class="row">
+                <div class="col-md-8"><select name="company" class="form-select">
                 <option value="" checked>Select One</option>
 
                 <?php 
@@ -48,20 +48,26 @@ $db = new mysqli("localhost", "root", "", "evidence_1");
                 <?php 
                 endwhile;
                 ?>
-            </select>
-            <input type="submit" value="DELETE" name="delete" class="btn btn-danger">
+                </select>
+            </div>
+                <div class="col-md-4">
+                    <input type="submit" value="DELETE MANUFACTURAR" name="delete" class="btn btn-danger">
+                </div>
+            </div>
+            
+            
         </form>
     </div>
 
     <div class="container">
-    <h3>Product List</h3>
+    <h3 style="text-align:center;">Product List</h3>
     </div>
     <div class="container">
         <?php 
         $sql = "SELECT * FROM product_details_view WHERE p_price>5000";
         $result = $db->query($sql); ?>
 
-        <table class="table table-striped">
+        <table class="table table-success table-striped">
             <tr>
                 <th>ID</th>
                 <th>Product Name</th>
@@ -84,7 +90,13 @@ $db = new mysqli("localhost", "root", "", "evidence_1");
         </table>
     </div>
     <div class="container">
-    <a href="entry.php" class="btn btn-dark" target="_blank">Add New Manufacturar</a>
+    <a href="entry.php" class="btn btn-success" target="_blank">Click to Add New Manufacturar</a>
     </div>
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 </body>
 </html>
