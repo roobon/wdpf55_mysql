2082d9c2c700011620cc8291bf68c1acb63b22cb
Ikbal Hossain <ikbalhossain125@gmail.com>
2023-09-02 01:01:45 +0600

    updated

---
+++ Ikbal_Hossain_1277699/Home_Practice/4_Super_Global/GET/Another_page/$_GET.php
@@ -0,0 +1,7 @@
+
+<!-- by default form method is GET -->
+
+<form action="2.1_action.php" method="get">
+    <input type="text" name="ename" placeholder="Enter Name"> <br> <br>
+    <input type="submit" name="sub" value="SUBMIT">
+</form> 
